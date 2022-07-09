f993fe1907d1b748d37d1a3092cd2ed9e519ffe5
Smiley <jcsmileyjr@gmail.com>
2022-07-08 21:07:37 -0500

    Build out the Start page header UI

--- src/pages/start/Start.js
+++ src/pages/start/Start.js
@@ -1,9 +1,23 @@
 import './start.css';
-
+import React, {useState} from 'react';
 
 const Start = ()=> {
     return(
-        <main>Hello World, Start page</main>
+        <main>
+            <section className='start__section--container'>
+                <div className='start__display--container'>
+                    <div className='start__header--container'>
+                        <p className='header__title--style'>Articles Read</p>
+                        <p className='header__content--style'>4</p>
+                    </div>
+                    <div className='start__header--container'>
+                        <p className='header__title--style'>Money Earned</p>
+                        <p className='header__content--style'>$7</p>
+                    </div>
+                </div>
+                <p className='header__text--style'>Get paid for reading and sharing your honest intelligent thoughts.</p>
+            </section>
+        </main>
     );
 }
 
