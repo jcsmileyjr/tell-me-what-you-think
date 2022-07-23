2338c3c047ce438ec391e4b9b191d3e870ea7b9f
Smiley <jcsmileyjr@gmail.com>
2022-07-23 12:18:27 -0500

    Refactor Thank You page to include takeaway for each story

--- src/pages/thankYou/ThankYou.js
+++ src/pages/thankYou/ThankYou.js
@@ -1,6 +1,6 @@
 import './thankYou.css';
 
-const ThankYou = ({next, done, moneyMade}) => {
+const ThankYou = ({next, done, moneyMade, takeaway}) => {
     const updateAppData = () => {
         let storyNumber = JSON.parse(localStorage.getItem('currentStory'));
         localStorage.setItem('currentStory', JSON.stringify(storyNumber + 1));
@@ -10,11 +10,12 @@ const ThankYou = ({next, done, moneyMade}) => {
         <main className='thankyou__main--container'>
             <h1 className='thankYou__header--style'>You earned ${moneyMade} for sharing your thoughts</h1>
             <h3 className='thankYou__title--style'>My takeaways on the article</h3>
-            <ul className='thankYou__content--style'>
+            {/* <ul className='thankYou__content--style'>
                 <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
                 <li>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </li>
                 <li>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </li>
-            </ul>
+            </ul> */}
+            <p className='thankYou__content--style'>{takeaway}</p>
 
             <p className='thankYou__instructions--style'>Click the button below to send an email to be paid.</p>
             <div className='thoughts__button--container'>
