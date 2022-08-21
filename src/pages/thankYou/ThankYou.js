380c04fc0d09e58abffe8decf403280b00ecce91
Smiley <jcsmileyjr@gmail.com>
2022-08-21 15:31:27 -0500

    Documentation of the "Thank you" page

--- src/pages/thankYou/ThankYou.js
+++ src/pages/thankYou/ThankYou.js
@@ -1,7 +1,16 @@
 import './thankYou.css';
 import Swal from 'sweetalert2'
 
+/**
+ * Final page of the app that displays the takeaway of the current story and amount of money earned for reading.
+ * @param {function} next - Function to redirect the user to the "Start" page.
+ * @param {function} done - function to update the current article object, update the articles array in local storage, and etc.
+ * @param {number} moneyMade - The amount of money earned from reading this article.
+ * @param {string} takeaway - Text from the current article object about the story.
+ */
+
 const ThankYou = ({next, done, moneyMade, takeaway}) => {
+    // function to set the next story and run the finished() fuction when the user press the "Finished" button
     const updateAppData = () => {
         let storyNumber = JSON.parse(localStorage.getItem('currentStory'));
         localStorage.setItem('currentStory', JSON.stringify(storyNumber + 1));
@@ -11,13 +20,7 @@ const ThankYou = ({next, done, moneyMade, takeaway}) => {
         <main className='thankyou__main--container'>
             <h1 className='thankYou__header--style'>You earned ${moneyMade} for sharing your thoughts</h1>
             <h3 className='thankYou__title--style'>My takeaways on the article</h3>
-            {/* <ul className='thankYou__content--style'>
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
-                <li>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </li>
-                <li>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </li>
-            </ul> */}
             <p className='thankYou__content--style'>{takeaway}</p>
-
             <p className='thankYou__instructions--style'>Click the button below to send an email to be paid.</p>
             <div className='thoughts__button--container'>
                 <button  onClick={() => {next("start"); updateAppData(); Swal.fire(`Let's earn more money! Read another article.`)}} className='thoughts__button--style'>FINISH</button>
