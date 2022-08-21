89bec555af9e1c23c544bd539cc2a38ad2dc9754
Smiley <jcsmileyjr@gmail.com>
2022-08-21 16:20:38 -0500

    Documentation of the Netlify getStories API

--- functions/getStories.js
+++ functions/getStories.js
@@ -1,6 +1,11 @@
 const sanityClient = require("@sanity/client");
 const { projectId, dataSet } = process.env;
 
+/**
+ * This object is used when making a database API call to Sanity.io.
+ * @param {string} projectId - environment varible from Netlify that identify which project to pull data from Sanity.io.
+ * @param {string} dataSet - environment variable from Netlify that identify which dataset to pull data from Sanity.io.
+ */
 const client = sanityClient({
   projectId: projectId,
   dataset: dataSet,
@@ -8,6 +13,10 @@ const client = sanityClient({
   useCdn: true, // `false` if you want to ensure fresh data
 });
 
+/**
+ * Function that query a database at Sanity.io for an array of article objects and adds an "end of array" object to this array
+ * @returns an array of article objects to be used by the app
+ */
 const getArticles = async () => {
   const query = '*[_type == "article"]';
   let onlineData = await client.fetch(query).then((articles) => {
@@ -19,6 +28,7 @@ const getArticles = async () => {
   return await updatedData;
 };
 
+// function that finds the "end of article" object and place it at the end of the array.
 const moveWarningArticleToEndOfArray = (articles) => {
   let foundWarningArticleIndex;
   articles.forEach((article, index) => {
@@ -33,7 +43,10 @@ const moveWarningArticleToEndOfArray = (articles) => {
   return articles
 }
 
-
+/**
+ * Netlify API endpoint
+ * @returns an data object with the array of article objects within the data property.
+ */
 exports.handler = async function () {
   var testData = await getArticles();
   return {
