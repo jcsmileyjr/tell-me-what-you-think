eea732d28c2d6bb88a3bf1ea16bf3dff5d4fa015
Smiley <jcsmileyjr@gmail.com>
2022-08-21 13:20:30 -0500

    Documentation: Update comments

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import Start from "./pages/start/Start";
 import ThankYou from "./pages/thankYou/ThankYou";
 import React, { useState, useEffect } from "react";
 //import emailjs from 'emailjs-com';
-import emailjs from '@emailjs/browser';
+import emailjs from "@emailjs/browser";
 
+// Object used as default for articles before the Sanity.io database API call and when the reader hit the last story
 const outOfStories = {
   title: "Warning",
   story:
@@ -14,6 +15,17 @@ const outOfStories = {
   paid: true,
 };
 
+/**
+ * Handles the business logic of the app
+ * @property {string} currentPage - String within the redirectUser() function to move the user between the Start and Thank You pages
+ * @property {array} stories - Array of objects with a reading article and related UI data
+ * @property {object} currentStorey - Current object from the stories array containing the article and related UI data
+ * @property {number} storiesRead - Current total of stories read used to track which article object to be shown to the user
+ * @property {number} amountEarned - Total of all articles read dollar amount with the calculateMoneyEarnedBeforePay() function
+ * @property {string} currentTitle - Curent title from the article object within the currentStory state
+ * @property {string} currentTakeaway - Current takeaway text from the article object withn the currentStory state
+ * @property {string} userThoughts - Text type in by the user into the textarea to be sent via email
+ */
 function App() {
   const [currentPage, setCurrentPage] = useState("");
   const [stories, setStories] = useState([outOfStories]);
@@ -24,28 +36,50 @@ function App() {
   const [currentTakeaway, setCurrentTakeaway] = useState("");
   const [userThoughts, setUserThoughts] = useState("");
 
+  //When the app first loads, gets data via an API call and loads it to the app's state. Move the user to the "Start" page
   useEffect(() => {
     setupApp();
     setCurrentPage("start");
   }, []);
 
+  // Cycle through all stories and if read but not marked as paid, display to the user the total amount earned.
   useEffect(() => {
     calculateMoneyEarnedBeforePay();
   });
 
+  /**
+   * Function to email the data typed by the user (userThoughts state) using the EmailJS service & libary
+   * @property {object} templateParams - data use by the EmailJS service to content via email
+   * @property {string} service_id - email service id provided by EmailJS and setup via the online service. In a .env file & used by Nelify
+   * @property {string} template_id - email template id provided by EmailJS and setup via the online service. In a .env file & used by Nelify
+   * @property {string} user_id - user id provided by EmailJS and setup via the online service. In a .env file & used by Nelify
+   */
   const emailMessage = () => {
     var templateParams = {
-      name:'Ameerah Salha',
-      message: `${userThoughts}`
+      name: "Ameerah Salha",
+      message: `${userThoughts}`,
     };
-    emailjs.send(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, templateParams, process.env.REACT_APP_USER_ID)
-    .then(function(response) {
-      console.log('SUCCESS!', response.status, response.text);
-   }, function(error) {
-      console.log('FAILED...', error);
-   });
-  }
+    emailjs
+      .send(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        templateParams,
+        process.env.REACT_APP_USER_ID
+      )
+      .then(
+        function (response) {
+          console.log("SUCCESS!", response.status, response.text);
+        },
+        function (error) {
+          console.log("FAILED...", error);
+        }
+      );
+  };
 
+  /**
+   * Function call when the "Finish" button on the Thank You page is pressed. It update the current article object,
+   * the amount of money earned, update local storage of the articles array, email the user thoughts, and reset the userthoughts state.
+   */
   const finishArticle = () => {
     let allStories = stories;
     allStories[storiesRead].read = true;
@@ -61,16 +95,21 @@ function App() {
     emailMessage();
   };
 
+  // Cycle through all stories and if read but not marked as paid, display to the user the total amount earned.
   const calculateMoneyEarnedBeforePay = () => {
     let totalAmount = 0;
     stories.forEach((article) => {
-      if (article.read && (article.paid === false || article.paid === undefined)) {
+      if (
+        article.read &&
+        (article.paid === false || article.paid === undefined)
+      ) {
         totalAmount += article.price;
       }
     });
     setAmountEarned(totalAmount);
   };
 
+  // Called withi the finishedArticle(), this updates the storiesRead, currentStory, currentTitle, and currentTakeaway state
   const getNextArticle = () => {
     const nextStoryNumber = storiesRead + 1;
     setStoriesRead(nextStoryNumber);
@@ -80,18 +119,20 @@ function App() {
     setCurrentTakeaway(stories[nextStoryNumber].takeaway);
   };
 
+  // Use a Netlify API call to the Sanity.io database to update the app's data based on if the current data in local storage is fresh
   const setupApp = async () => {
-
     fetch(".netlify/functions/getStories")
       .then((response) => response.json())
       .then((json) => {
         let data = json.data;
-        let previousSavedStories = localStorage.getItem("TellMeWhatYouThink-Content");
+        let previousSavedStories = localStorage.getItem(
+          "TellMeWhatYouThink-Content"
+        );
         if (
           previousSavedStories !== null &&
           previousSavedStories.length >= data.length
         ) {
-          data = JSON.parse(previousSavedStories) ;
+          data = JSON.parse(previousSavedStories);
           console.log("saved data overwritten newly loaded data");
         }
         setStories(data);
@@ -127,10 +168,9 @@ function App() {
     setCurrentPage(page);
   };
 
-  /**
-   * Current amount offer to be paid if child read the story
-   */
+  // Current amount offer to be paid if child read the story
   let storyPrice = stories[storiesRead].price;
+
   return (
     <div className="container">
       <div className="rain">
@@ -142,7 +182,7 @@ function App() {
             numberOfStoriesRead={storiesRead}
             totalEarned={amountEarned}
             getUserThoughts={setUserThoughts}
-            currentUserThoughts = {userThoughts}            
+            currentUserThoughts={userThoughts}
           />
         )}
         {currentPage === "thankyou" && (
