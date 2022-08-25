e21e8594cca5d7e60fac86de98565c69d958e25b
Smiley <jcsmileyjr@gmail.com>
2022-08-25 07:28:18 -0500

    Documentation: Linting the main pages

--- src/pages/start/Start.js
+++ src/pages/start/Start.js
@@ -1,10 +1,10 @@
-import './start.css';
-import React, {useState} from 'react';
-import Swal from 'sweetalert2' 
+import "./start.css";
+import React, { useState } from "react";
+import Swal from "sweetalert2";
 
 /**
  * Primary page of the app use to display the article to be read and a text area to record the user typed thoughts
- * 
+ *
  * @param {function} next - Function that moves the user to the "Thank You" page.
  * @param {array} userStories - The original article has been broken into an array of text.
  * @param {number} numberOfStoriesRead - Total number of stories previously read.
@@ -12,128 +12,173 @@ import Swal from 'sweetalert2'
  * @param {string} storyTitle - Title of the current article object
  * @param {function} getUserThoughts - Function use to record the user thoughts from the text area element
  * @param {string} currentUserThoughts - Recorded text from the text area by the user
- * @returns 
+ * @returns
  */
-const Start = ({next, userStories, numberOfStoriesRead, totalEarned, storyTitle, getUserThoughts, currentUserThoughts})=> {
-    const [scaleHeader, setScaleHeader] = useState(false);
-    const [hideHeader, setHideHeader] = useState(false);
-    const [nextParagragh, setNextParagraph] = useState(0);
-    const [storyBook, setStoryBook] = useState([]);
-    const [numberOfPages, setNumberOfPages] = useState(0);
+const Start = ({
+  next,
+  userStories,
+  numberOfStoriesRead,
+  totalEarned,
+  storyTitle,
+  getUserThoughts,
+  currentUserThoughts,
+}) => {
+  const [scaleHeader, setScaleHeader] = useState(false);
+  const [hideHeader, setHideHeader] = useState(false);
+  const [nextParagragh, setNextParagraph] = useState(0);
+  const [storyBook, setStoryBook] = useState([]);
+  const [numberOfPages, setNumberOfPages] = useState(0);
 
-    // Take the current article and condense several array text into an paragraph to be displayed
-    React.useEffect(() => {
-        breakUpStory();
-    }, [userStories]);
+  // Take the current article and condense several array text into an paragraph to be displayed
+  React.useEffect(() => {
+    breakUpStory();
+  }, [userStories]);
 
-    /**
-     * Function to condense several array text (from the current article stories property) into paragraphs to be displayed. 
-     * Notes on how to do this function at: https://domhabersack.com/chunking-arrays
-     */
-    const breakUpStory = () => {
-        let article = userStories;
-        const numberOfChunks = Math.ceil(article.length / 4)
-        let book = [...Array(numberOfChunks)].map((arr, index) => {   
-                return article.slice(index * 4, (index + 1) * 4);
-        })
-        setNumberOfPages(book.length);
-        setStoryBook(book);
+  /**
+   * Function to condense several array text (from the current article stories property) into paragraphs to be displayed.
+   * Notes on how to do this function at: https://domhabersack.com/chunking-arrays
+   */
+  const breakUpStory = () => {
+    let article = userStories;
+    const numberOfChunks = Math.ceil(article.length / 4);
+    let book = [...Array(numberOfChunks)].map((arr, index) => {
+      return article.slice(index * 4, (index + 1) * 4);
+    });
+    setNumberOfPages(book.length);
+    setStoryBook(book);
+  };
+
+  /**
+   * function that displays an UI of a paragraph from the current article or an "End of Article" message
+   *
+   * @returns a horizontal UI of sentences
+   */
+  const displayStory = () => {
+    let book = ["Waiting"]; // default text if there is an error
+    if (storyBook.length !== 0) {
+      book = storyBook[nextParagragh];
     }
+    const article = book.map((line, index) => {
+      if (index !== book.length - 1) {
+        return (
+          <React.Fragment key={index}>
+            {line}.{userStories.length - 1 === index ? "" : <br />}{" "}
+            {/* Line break */}
+          </React.Fragment>
+        );
+      } else {
+        return (
+          <React.Fragment key={index}>
+            {line}
+            {userStories.length - 1 === index ? "" : <br />}
+            {nextParagragh === numberOfPages - 1 && (
+              <div className="conversation__endOfArticle--container">
+                <hr className="style-seven" />
+                <p className="conversation__endOfArticle--style">
+                  📑End of Article📑
+                </p>
+                <hr className="style-seven" />
+              </div>
+            )}
+            <div className="conversation__nextPage--container">
+              <button
+                onClick={() => getNextParagraph()}
+                className="conversation__nextPage--style"
+              >
+                Read More
+              </button>
+            </div>
+          </React.Fragment>
+        );
+      }
+    });
+    return article;
+  };
 
-    /**
-     * function that displays an UI of a paragraph from the current article or an "End of Article" message
-     * 
-     * @returns a horizontal UI of sentences
-     */
-    const displayStory = () => {
-        let book = ["Waiting"]; // default text if there is an error
-        if(storyBook.length !== 0){
-            book = storyBook[nextParagragh];
-        }
-        const article = book.map((line, index) => {
-            if(index !== book.length - 1){
-                return(
-                    <React.Fragment key={index}>
-                        {line}.
-                        {userStories.length - 1 === index?"":<br/>} {/* Line break */}
-                    </React.Fragment>
-                )
-            }else{
-                return(
-                    <React.Fragment key={index}>
-                        {line}
-                        {userStories.length - 1 === index?"":<br/>}
-                        {nextParagragh === numberOfPages -1 &&
-                            <div className='conversation__endOfArticle--container'>
-                                <hr className="style-seven" />
-                                <p className='conversation__endOfArticle--style'>📑End of Article📑</p>
-                                <hr className="style-seven" />
-                            </div>
-                        }
-                        <div className='conversation__nextPage--container'>
-                            <button onClick={()=> getNextParagraph()} className='conversation__nextPage--style'>Read More</button>
-                        </div>
-                    </React.Fragment>
-                )
+  // Function to update the nextParagraph state that faciliate assigning the next paragraph in the storybook state to display.
+  const getNextParagraph = () => {
+    if (nextParagragh >= storyBook.length - 1) {
+      setNextParagraph(0);
+    } else {
+      setNextParagraph((prevNumber) => prevNumber + 1);
+    }
+  };
+
+  // function to hide the header section (displayed amount earned & stories read) when user is typing in the text area.
+  const hideSection = () => {
+    setScaleHeader(true);
+    const interval = setInterval(() => {
+      setHideHeader(true);
+      clearInterval(interval);
+    }, 500);
+  };
+
+  // function to show the header section (displayed amount earned & stories read) when user is typing in the text area.
+  const showSection = () => {
+    setScaleHeader(false);
+    const interval = setInterval(() => {
+      setHideHeader(false);
+      clearInterval(interval);
+    }, 500);
+  };
+
+  return (
+    <main>
+      <section
+        className={`start__section--container ${
+          scaleHeader ? "scale-out-center" : "scale-in-center"
+        } ${hideHeader ? "hide" : "show"}`}
+      >
+        <div className="start__display--container">
+          <div className="start__header--container">
+            <p className="header__title--style">Articles Read</p>
+            <p className="header__content--style">{numberOfStoriesRead}</p>
+          </div>
+          <div className="start__header--container">
+            <p className="header__title--style">Money Earned</p>
+            <p className="header__content--style">${totalEarned}</p>
+          </div>
+        </div>
+        <p className="header__text--style">
+          Get paid for reading and sharing your honest intelligent thoughts.
+        </p>
+      </section>
+      <section className="start__section--container">
+        <p className="conversation__header--style">Today's Conversation</p>
+        <h1 className="conversation__title--style">{storyTitle}</h1>
+        <div className="conversation__content--style">{displayStory()}</div>
+      </section>
+      <section className="start__section--container">
+        <label className="thoughts__header--style">
+          What are your thoughts
+          <textarea
+            onChange={(e) => getUserThoughts(e.target.value)}
+            onFocus={() => hideSection()}
+            onBlur={() => showSection()}
+            className="thoughts__textarea--style"
+            id="thoughts"
+            rows="12"
+          />
+        </label>
+        <div className="thoughts__button--container">
+          <button
+            disabled={
+              currentUserThoughts === "" || storyTitle === "Warning"
+                ? true
+                : false
             }
-        })
-        return article
-    }
-
-    // Function to update the nextParagraph state that faciliate assigning the next paragraph in the storybook state to display.
-    const getNextParagraph = () => {
-        if(nextParagragh >= (storyBook.length - 1)){
-            setNextParagraph(0);
-        }else{
-            setNextParagraph(prevNumber => prevNumber + 1);
-        }
-    }
-
-    // function to hide the header section (displayed amount earned & stories read) when user is typing in the text area.
-    const hideSection = () => {
-        setScaleHeader(true);
-        const interval = setInterval(() => {setHideHeader(true); clearInterval(interval)},500);
-    }
-
-    // function to show the header section (displayed amount earned & stories read) when user is typing in the text area.
-    const showSection = () => {
-        setScaleHeader(false);
-        const interval = setInterval(() => {setHideHeader(false); clearInterval(interval)},500);
-    }
-    
-    return(
-        <main>
-            <section className={`start__section--container ${scaleHeader? 'scale-out-center':"scale-in-center"} ${hideHeader?'hide':'show'}`}>
-                <div className='start__display--container'>
-                    <div className='start__header--container'>
-                        <p className='header__title--style'>Articles Read</p>
-                        <p className='header__content--style'>{numberOfStoriesRead}</p>
-                    </div>
-                    <div className='start__header--container'>
-                        <p className='header__title--style'>Money Earned</p>
-                        <p className='header__content--style'>${totalEarned}</p>
-                    </div>
-                </div>
-                <p className='header__text--style'>Get paid for reading and sharing your honest intelligent thoughts.</p>
-            </section>
-            <section className='start__section--container'>
-                <p className='conversation__header--style'>Today's Conversation</p>
-                <h1 className='conversation__title--style'>{storyTitle}</h1>
-                <div className='conversation__content--style'>
-                    {displayStory()}         
-                </div>
-
-            </section>
-            <section className='start__section--container'>
-                <label className='thoughts__header--style'>What are your thoughts
-                    <textarea onChange={(e)=> getUserThoughts(e.target.value)} onFocus={() => hideSection()} onBlur={() => showSection()} className='thoughts__textarea--style' id="thoughts" rows="12" />
-                </label>
-                <div className='thoughts__button--container'>
-                    <button disabled={currentUserThoughts==="" || storyTitle === "Warning"?true:false} onClick={() => {next("thankyou"); Swal.fire('Thank you for sharing your thoughts!');} } className='thoughts__button--style'>NEXT</button>
-                </div>
-            </section>
-        </main>
-    );
-}
+            onClick={() => {
+              next("thankyou");
+              Swal.fire("Thank you for sharing your thoughts!");
+            }}
+            className="thoughts__button--style"
+          >
+            NEXT
+          </button>
+        </div>
+      </section>
+    </main>
+  );
+};
 
 export default Start;
--- src/pages/thankYou/ThankYou.js
+++ src/pages/thankYou/ThankYou.js
@@ -1,33 +1,46 @@
-import './thankYou.css';
-import Swal from 'sweetalert2'
+import "./thankYou.css";
+import Swal from "sweetalert2";
 
 /**
  * Final page of the app that displays the takeaway of the current story and amount of money earned for reading.
- * 
+ *
  * @param {function} next - Function to redirect the user to the "Start" page.
  * @param {function} done - function to update the current article object, update the articles array in local storage, and etc.
  * @param {number} moneyMade - The amount of money earned from reading this article.
  * @param {string} takeaway - Text from the current article object about the story.
  */
 
-const ThankYou = ({next, done, moneyMade, takeaway}) => {
-    // function to set the next story and run the finished() fuction when the user press the "Finished" button
-    const updateAppData = () => {
-        let storyNumber = JSON.parse(localStorage.getItem('currentStory'));
-        localStorage.setItem('currentStory', JSON.stringify(storyNumber + 1));
-        done();
-    }
-    return(
-        <main className='thankyou__main--container'>
-            <h1 className='thankYou__header--style'>You earned ${moneyMade} for sharing your thoughts</h1>
-            <h3 className='thankYou__title--style'>My takeaways on the article</h3>
-            <p className='thankYou__content--style'>{takeaway}</p>
-            <p className='thankYou__instructions--style'>Click the button below to send an email to be paid.</p>
-            <div className='thoughts__button--container'>
-                <button  onClick={() => {next("start"); updateAppData(); Swal.fire(`Let's earn more money! Read another article.`)}} className='thoughts__button--style'>FINISH</button>
-            </div>
-        </main>
-    );
-}
+const ThankYou = ({ next, done, moneyMade, takeaway }) => {
+  // function to set the next story and run the finished() fuction when the user press the "Finished" button
+  const updateAppData = () => {
+    let storyNumber = JSON.parse(localStorage.getItem("currentStory"));
+    localStorage.setItem("currentStory", JSON.stringify(storyNumber + 1));
+    done();
+  };
+  return (
+    <main className="thankyou__main--container">
+      <h1 className="thankYou__header--style">
+        You earned ${moneyMade} for sharing your thoughts
+      </h1>
+      <h3 className="thankYou__title--style">My takeaways on the article</h3>
+      <p className="thankYou__content--style">{takeaway}</p>
+      <p className="thankYou__instructions--style">
+        Click the button below to send an email to be paid.
+      </p>
+      <div className="thoughts__button--container">
+        <button
+          onClick={() => {
+            next("start");
+            updateAppData();
+            Swal.fire(`Let's earn more money! Read another article.`);
+          }}
+          className="thoughts__button--style"
+        >
+          FINISH
+        </button>
+      </div>
+    </main>
+  );
+};
 
 export default ThankYou;
